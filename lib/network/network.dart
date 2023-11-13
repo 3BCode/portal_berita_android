25ba3b8cfca4524e1ed8bb653355733c92da5022
Riky yahdin <rikyyahdin@gmail.com>
2023-11-13 16:06:07 +0700

    minggu 5 menambahkan fungsi imageview profil user

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,5 +1,5 @@
 class NetworkURL {
-  static String server = "http://192.168.1.6/portal_berita";
+  static String server = "http://isi dengan url server/portal_berita";
 
   static String login() {
     return "$server/API/login.php";
